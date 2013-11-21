e84fcaf90cb5169da971b6f0e6d0ff25496f1aaf
Splintex <godun.yevgen@gmail.com>
2013-11-21 22:01:37 +0200

    menu

---
+++ js/common_new.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// toogle menu
+	var menu_key = $(".js-menu-key");
+	var menu_list = $(".js-menu-list");
+	menu_key.bind("click", function(){
+		menu_list.slideToggle("fast");
+	});
+});
